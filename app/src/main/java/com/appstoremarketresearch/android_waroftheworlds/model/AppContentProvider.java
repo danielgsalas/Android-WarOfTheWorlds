68e6a6edfcb6609dcd717884dfb33ce76d47528d
danielgsalas <danielgsalas@yahoo.com>
2016-10-08 10:42:19 -0700

    added license to comment headers

--- app/src/main/java/com/appstoremarketresearch/android_waroftheworlds/model/AppContentProvider.java
+++ app/src/main/java/com/appstoremarketresearch/android_waroftheworlds/model/AppContentProvider.java
@@ -9,17 +9,31 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.net.Uri;
 
 /**
- * AppContentProvider provides access to the application database.
+ * <p>AppContentProvider provides access to the application database.</p>
  *
- * @author <a href="http://www.appstoremarketresearch.com">
- * http://www.appstoremarketresearch.com</a>
+ * <p>Copyright 2016 <a href="http://www.appstoremarketresearch.com">
+ * http://www.appstoremarketresearch.com</a></p>
+ *
+ * <p>This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.</p>
+ *
+ * <p>This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.</p>
+ *
+ * <p>You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <a href="http://www.gnu.org/licenses">
+ * http://www.gnu.org/licenses</a>.</p>
  */
 public class AppContentProvider extends ContentProvider {
 
-	private SQLiteOpenHelper dbHelper;
+    private SQLiteOpenHelper dbHelper;
 
-	public static final String AUTHORITY = 
-		AppContentProvider.class.getCanonicalName();
+    public static final String AUTHORITY = 
+        AppContentProvider.class.getCanonicalName();
 
 	public static final Uri CONTENT_URI_MARTIAN =
 		Uri.parse("content://" + AUTHORITY + "/martian");
@@ -27,75 +41,75 @@ public class AppContentProvider extends ContentProvider {
 	public static final Uri CONTENT_URI_MARTIAN_TRIPOD =
 		Uri.parse("content://" + AUTHORITY + "/martian_tripod");
 
-	@Override
-	public int delete (
-		Uri uri,
-		String selection,
-		String[] selectionArgs) {
+    @Override
+    public int delete (
+        Uri uri,
+        String selection,
+        String[] selectionArgs) {
 
-		SQLiteDatabase db = dbHelper.getWritableDatabase();
-		String uriText = uri.toString();
-		String tablename = uriText.substring(uriText.lastIndexOf("/")+1);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        String uriText = uri.toString();
+        String tablename = uriText.substring(uriText.lastIndexOf("/")+1);
 
-		return db.delete(tablename, selection, selectionArgs);
-	}
+        return db.delete(tablename, selection, selectionArgs);
+    }
 
-	@Override
-	public String getType (Uri uri) {
-		return null;
-	}
+    @Override
+    public String getType (Uri uri) {
+        return null;
+    }
 
-	@Override
-	public Uri insert (
-		Uri uri,
-		ContentValues values) {
+    @Override
+    public Uri insert (
+        Uri uri,
+        ContentValues values) {
 
-		SQLiteDatabase db = dbHelper.getWritableDatabase();
-		String uriText = uri.toString();
-		String tablename = uriText.substring(uriText.lastIndexOf("/")+1);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        String uriText = uri.toString();
+        String tablename = uriText.substring(uriText.lastIndexOf("/")+1);
 
-		long id =  db.insert(tablename, null, values);
-		getContext().getContentResolver().notifyChange(uri, null);
-		return ContentUris.withAppendedId(uri, id);
-	}
+        long id =  db.insert(tablename, null, values);
+        getContext().getContentResolver().notifyChange(uri, null);
+        return ContentUris.withAppendedId(uri, id);
+    }
 
-	@Override
-	public boolean onCreate () {
-		dbHelper = new AppDatabaseHelper(getContext());
-		return true; // assume provider was successfully loaded
-	}
+    @Override
+    public boolean onCreate () {
+        dbHelper = new AppDatabaseHelper(getContext());
+        return true; // assume provider was successfully loaded
+    }
 
-	@Override
-	public Cursor query (
-		Uri uri,
-		String[] projection,
-		String selection,
-		String[] selectionArgs,
-		String sortOrder){
+    @Override
+    public Cursor query (
+        Uri uri,
+        String[] projection,
+        String selection,
+        String[] selectionArgs,
+        String sortOrder){
 
-		SQLiteDatabase db = dbHelper.getWritableDatabase();
-		String uriText = uri.toString();
-		String tablename = uriText.substring(uriText.lastIndexOf("/")+1);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        String uriText = uri.toString();
+        String tablename = uriText.substring(uriText.lastIndexOf("/")+1);
 
-		String groupBy = null;
-		String having = null;
+        String groupBy = null;
+        String having = null;
 
-		return db.query(tablename, projection,
-			selection, selectionArgs,
-			groupBy, having, sortOrder);
-	}
+        return db.query(tablename, projection,
+            selection, selectionArgs,
+            groupBy, having, sortOrder);
+    }
 
-	@Override
-	public int update (
-		Uri uri,
-		ContentValues values,
-		String selection,
-		String[] selectionArgs) {
+    @Override
+    public int update (
+        Uri uri,
+        ContentValues values,
+        String selection,
+        String[] selectionArgs) {
 
-		SQLiteDatabase db = dbHelper.getWritableDatabase();
-		String uriText = uri.toString();
-		String tablename = uriText.substring(uriText.lastIndexOf("/")+1);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        String uriText = uri.toString();
+        String tablename = uriText.substring(uriText.lastIndexOf("/")+1);
 
-		return db.update(tablename, values, selection, selectionArgs);
-	}
+        return db.update(tablename, values, selection, selectionArgs);
+    }
 }
--- app/src/main/java/com/appstoremarketresearch/android_waroftheworlds/model/AppDatabaseHelper.java
+++ app/src/main/java/com/appstoremarketresearch/android_waroftheworlds/model/AppDatabaseHelper.java
@@ -8,45 +8,59 @@ import android.database.sqlite.SQLiteOpenHelper;
 import com.appstoremarketresearch.android_waroftheworlds.R;
 
 /**
- * AppDatabaseHelper helps create, access and open the application database.
+ * <p>AppDatabaseHelper helps create, access and open the application database.</p>
  *
- * @author <a href="http://www.appstoremarketresearch.com">
- * http://www.appstoremarketresearch.com</a>
+ * <p>Copyright 2016 <a href="http://www.appstoremarketresearch.com">
+ * http://www.appstoremarketresearch.com</a></p>
+ *
+ * <p>This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.</p>
+ *
+ * <p>This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.</p>
+ *
+ * <p>You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <a href="http://www.gnu.org/licenses">
+ * http://www.gnu.org/licenses</a>.</p>
  */
 public class AppDatabaseHelper extends SQLiteOpenHelper {
 
-	private Context context;
+    private Context context;
 
-	public static final String DB_NAME = "com.appstoremarketresearch.android_waroftheworlds.db";
-	public static final int DB_VERSION = 1;
+    public static final String DB_NAME = "com.appstoremarketresearch.android_waroftheworlds.db";
+    public static final int DB_VERSION = 1;
 
-	public AppDatabaseHelper(Context context) {
-		super(context, DB_NAME, null, DB_VERSION);
-		this.context = context;
-	}
+    public AppDatabaseHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+        this.context = context;
+    }
 
-	@Override
-	public void onCreate (SQLiteDatabase db) {
-		Resources res = context.getResources();
+    @Override
+    public void onCreate (SQLiteDatabase db) {
+        Resources res = context.getResources();
 		db.execSQL(res.getString(R.string.sql_create_table_martian));
 		db.execSQL(res.getString(R.string.sql_create_table_martian_tripod));
-	}
+    }
 
-	@Override
-	public void onUpgrade (
-		SQLiteDatabase db,
-		int oldVersion,
-		int newVersion) {
+    @Override
+    public void onUpgrade (
+        SQLiteDatabase db,
+        int oldVersion,
+        int newVersion) {
 
-		switch(oldVersion) {
-		case 1:
-			// TBD
-			break;
+        switch(oldVersion) {
+        case 1:
+            // TBD
+            break;
 
-		case 2:
-		default:
-			// TBD
-			break;
-		}
-	}
+        case 2:
+        default:
+            // TBD
+            break;
+        }
+    }
 }
